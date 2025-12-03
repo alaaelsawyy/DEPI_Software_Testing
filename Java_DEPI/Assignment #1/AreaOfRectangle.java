5fe5193c93be024fece1ff882ae7f0bfe5cb9079
Alaa El Sawy <alaaelsawy927@gmail.com>
2025-12-03 21:20:25 +0200

    Add files via upload

---
+++ Java_DEPI/Assignment #1/AreaOfRectangle.java
@@ -0,0 +1,14 @@
+// Write a program that calculate area of a rectangle given width and height (integers).
+
+package My_Package;
+
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+
+        int height = 5;
+        int width = 6;
+        int area = height * width;
+
+        System.out.println("Area = " + area);
+    }
+}
